79edd8acf25c717da2f7f96d6af29280c766f9f8
ethanhuang218 <ethanhuang218@gmail.com>
2025-04-03 08:23:34 -0400

    feat: add tracking

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import HeroSection from "./components/HeroSection";
 import TimelineSection from "./components/TimelineSection";
 import ContactSection from "./components/ContactSection";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => (
   <div>
+    <Analytics />
     <HeroSection />
     <TimelineSection />
     <ContactSection />
